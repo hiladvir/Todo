de73b7695864ab63bf7abb21e525bbd630981bbf
hafifa1 <hiladvir14@gmail.com>
2024-01-28 12:49:13 +0200

    tal fixed another mistake

--- app/src/main/java/com/example/mytest/AlarmReceiver.java
+++ app/src/main/java/com/example/mytest/AlarmReceiver.java
@@ -1,20 +1,31 @@
 package com.example.mytest;
-import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
-import java.util.Date;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-       Toast.makeText(context, "תזכורת!!!", Toast.LENGTH_LONG).show();
-       Intent serviceIntent = new Intent(context,MyService.class);
-       context.startService(serviceIntent);
-    }
+        showNotification(context, "תזכורת");
+        }
 
+    private void showNotification(Context context, String title) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService
+                (Context.NOTIFICATION_SERVICE);
+        String channelId = "my_channel_id";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,"My Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(android.R.drawable.ic_dialog_info).setContentTitle(title)
+                .setContentText(title).setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(1, builder.build());
+    }
 }
